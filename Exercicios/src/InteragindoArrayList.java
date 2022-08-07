7a0252b28a5d4337566629981a951d37f1c8172e
Gismi Guimarães <97984496+Gismii@users.noreply.github.com>
2022-08-07 02:20:07 -0300

    Conclusão do modulo1

--- Exercicios/src/InteragindoArrayList.java
+++ Exercicios/src/InteragindoArrayList.java
@@ -8,35 +8,45 @@ public class InteragindoArrayList {
 
         Scanner input = new Scanner(System.in);
         ArrayList<Double> lista = new ArrayList<>();
-        double media = lista.get(2);
-
-
+        double soma=0;
         double numero;
+        double media=0;
+
+
+        //double numero;
         while (true) {
             System.out.print("Digite um numero: ");
             numero = input.nextDouble();
+            //soma=numero;
 
             if (numero == 0) {
                 break;
             }
             lista.add(numero);
+            }
+            double maior, menor;
+            maior = menor = lista.get(0);
+
+            for (Double valor : lista) {
+                if (valor > maior) {
+                    maior = valor;
+                } else if (valor < menor) {
+                    menor = valor;
+
+                }
+
+            }  for (int i = 0; i <lista.size(); i++) {
+            soma+=lista.get(i);
+
         }
-        double maior, menor;
-        maior = menor  = lista.get(0);
-
-        for (Double valor: lista) {
-            if (valor > maior) {
-                maior = valor;
-            } else if (valor < menor) {
-                menor = valor;
-
+                System.out.println("maior " + maior);
+                System.out.println("menor " + menor);
+                System.out.println("Media dos mumero digitados : " + soma/ lista.size());
             }
         }
 
-        System.out.println("maior " + maior);
-        System.out.println("menor " + menor);
-        //System.out.println("Media " + media );
-    }
-}
+
+
+
 
 
